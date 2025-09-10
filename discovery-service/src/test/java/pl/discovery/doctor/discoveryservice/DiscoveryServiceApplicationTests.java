991fbf9995fa8fa4d49450def68cdcb7302903e7
damian <damian.duda@gmail.com>
2025-09-10 16:10:57 +0200

    Dodanie pierwszej wersji serwera discovery

---
+++ discovery-service/src/test/java/pl/discovery/doctor/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.discovery.doctor.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
